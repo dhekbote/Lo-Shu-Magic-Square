a7aeaa4830c86319bc2485e75faf1cd357379fe7
dhekbote <dhekbote@cpp.edu>
2023-10-27 23:21:46 -0700

    Added loShuMagicSquare.h

---
+++ loShuMagicSquare.h
@@ -0,0 +1,13 @@
+#ifndef LOSHUMAGICSQUARE_H
+#define LOSHUMAGICSQUARE_H
+
+// Function to generate a Lo Shu Magic Square
+void generateSquare(int matrix[3][3]);
+
+// Function to check if a 2D array is a Lo Shu Magic Square
+int checkSquare(int matrix[3][3]);
+
+// Function to print a 3x3 matrix
+void printSquare(int matrix[3][3]);
+
+#endif
